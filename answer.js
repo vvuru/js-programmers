82dee2997bc47f0a105c4ccaf00ba35c43d70743
vvuru <wown5398@gmail.com>
2023-10-10 15:02:23 +0900

    문자열 출력하기 / 코딩 기초 트레이닝

--- answer.js
+++ answer.js
@@ -1,26 +1,17 @@
 /**
- * https://school.programmers.co.kr/learn/courses/30/lessons/120833
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181952
  */
-function solution(numbers, num1, num2) {
-    let answer = [];
-    for(let i=0; i<numbers.length; i++) {
-        if(i>=num1 && i<=num2) {
-            answer.push(numbers[i]);
-        }
-    }
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-    return answer;
-}
+let input = [];
 
-function solution2(numbers, num1, num2) {
-    return numbers.slice(num1,num2+1);
-}
-
-function solution3(numbers, num1, num2) {
-    return numbers.splice(num1,num2-num1+1);
-}
-
-const numbers = [1,2,3,4,5];
-const num1 = 1;
-const num2 = 3;
-console.log(solution3(numbers, num1, num2));
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    console.log(str)
+});
