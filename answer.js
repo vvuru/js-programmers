e9505e5b8d68ce411bf383799d31ab3007a4819a
vvuru <wown5398@gmail.com>
2023-10-11 23:57:39 +0900

    문자열 출력하기-붙여서/ 코딩 기초 트레이닝

--- answer.js
+++ answer.js
@@ -2,16 +2,15 @@
  * https://school.programmers.co.kr/learn/courses/30/lessons/181952
  */
 const readline = require('readline');
+let input = [];
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-});
-
-let input = [];
-
-rl.on('line', function (line) {
+}).on('line', function (line) {
     input = [line];
 }).on('close',function(){
     str = input[0];
     console.log(str)
 });
+
+
